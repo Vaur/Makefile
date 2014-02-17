b5c18b3363fd62667ff83fe5e85d47a0aa5948eb
devill_x <devill_x@epitech.eu>
2014-02-17 13:41:08 +0100

    color is now inside defines

--- include/debug.h
+++ include/debug.h
@@ -5,7 +5,7 @@
 // Login   <devill_x@epitech.net>
 //
 // Started on  Wed Jan 22 13:41:35 2014 Xavier Devilliers
-// Last update Fri Jan 24 16:12:22 2014 Xavier Devilliers
+// Last update Mon Feb 17 13:40:23 2014 devill_x
 //
 
 #ifndef		DEBUG_H
@@ -15,9 +15,20 @@
 ** define
 */
 
+/*
+** color
+*/
+
+# define	RED		"\E[31m\033[1m"
+# define	WHI		"\033[0m "
+
+/*
+** Macro debug
+*/
+
 # define	debug(msg)	if (DEBUG_ == 1)			\
-    std::cout << "\E[31m\033[1m" << __FILE__ <<				\
-      " \033[0m: \E[31m\033[1m" <<  __LINE__  << "\033[0m " << msg <<	\
+    std::cout <<  RED << __FILE__ <<	WHI <<				\
+      ": " << RED <<  __LINE__  << WHI << msg <<			\
       std::endl;
 
 #endif      /* !DEBUG_H_ */
